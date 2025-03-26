b3fb25327cc290389703d3be1cf1a18e9779ffec
lyonai <2366923087@qq.com>
2025-03-27 00:14:07 +0800

    Site updated: 2025-03-27 00:14:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/24/常见面试题/","2025/03/24/这是一篇新的博文/","2025/03/24/hello-world/"];function toRandomPost(){
+var posts=["2025/03/24/常见面试题/","2025/03/26/八股文详细版/","2025/03/24/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
